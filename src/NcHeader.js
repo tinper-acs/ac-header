543723db22551023af11c64ebdcf013343bf4e2e
liushaozhen <804199994@qq.com>
2019-07-23 18:22:24 +0800

    commit

---
+++ src/NcHeader.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+const propTypes = {};
+const defaultProps = {};
+class NcHeader extends Component {render(){return(<h2>Welcome use tinper-acs </h2> )}};
+NcHeader.propTypes = propTypes;
+NcHeader.defaultProps = defaultProps;
+export default NcHeader;
